54a00345de6d90f6bb8937e0a91917a0988ed329
Benedikt Arnold <benedikt.arnold@gmx.net>
2013-04-01 18:53:32 +0200

    use of ntohl and htonl from npm library instead of own implementation

--- owfs.js
+++ owfs.js
@@ -1,29 +1,9 @@
 var net = require('net'),
     sys = require('sys'),
-    events = require('events');
+    events = require('events'),
+    ntohl = require('network-byte-order').ntohl,
+    htonl = require('network-byte-order').htonl;
 require('buffertools');
-
-// Convert an integer to an array of "bytes" in network/big-endian order.
-function htonl(n)
-{
-    // Mask off 8 bytes at a time then shift them into place
-    return [
-        (n & 0xFF000000) >>> 24,
-        (n & 0x00FF0000) >>> 16,
-        (n & 0x0000FF00) >>>  8,
-        (n & 0x000000FF) >>>  0,
-    ];
-}
-
-function ntohl(b){
-      return ((0xff & b[0]) << 24) |
-         ((0xff & b[1]) << 16) |
-         ((0xff & b[2]) << 8) |
-         ((0xff & b[3]));
-}
-
-
-
 
 var header_props = ["version","payload", "ret", "controlflags", "size", "offset"];
 
@@ -47,11 +27,10 @@ function _send(path,fun, callback){
         callback(messages);
     })
     socket.on('data', function (data) {
-        var i,j=0,temparray,chunk = 4;
+        var i,j=0,chunk = 4;
         var header ={};
         for (i=0; i<24; i+=chunk) {
-            temparray = data.slice(i,i+chunk);
-            var value = ntohl(temparray);
+            var value = ntohl(data,i);
             header[header_props[j]] = value;
             j++;
         }
@@ -64,14 +43,14 @@ function _send(path,fun, callback){
     });
     socket.connect(this._port, this._server, function() {
         var data_len = 8192;
-        var msg =[];
-        msg = msg.concat(htonl(0));           //version
-        msg = msg.concat(htonl(path.length+1)); //payload length
-        msg = msg.concat(htonl(fun));    //type of function call -> http://owfs.org/index.php?page=owserver-message-types
-        msg = msg.concat(htonl(0x00000020));         //format flags -- 266 for alias upport
-        msg = msg.concat(htonl(data_len));    //size of data element for read or write
-        msg = msg.concat(htonl(0));  
-        var bres = buffertools.concat(new Buffer(msg), path+'\x00');
+        var msg = new Buffer(24);
+        htonl(msg,0,0);           //version
+        htonl(msg,4,path.length+1); //payload length
+        htonl(msg,8,fun);    //type of function call -> http://owfs.org/index.php?page=owserver-message-types
+        htonl(msg,12,0x00000020);         //format flags -- 266 for alias upport
+        htonl(msg,16,data_len);    //size of data element for read or write
+        htonl(msg,20,0);  
+        var bres = buffertools.concat(msg, path+'\x00');
         socket.end(bres);
     });
 }
