2a1c7b5db08cb1920ecf90c8cbb2dfbd894d0389
Benedikt Arnold <benedikt.arnold@gmx.net>
2014-08-28 10:46:25 +0200

    added examples and simplified the examples to seamless use owserver fake device on localhost

--- examples/debug.js
+++ examples/debug.js
@@ -2,7 +2,7 @@ var Client = require("../lib/owfs").Client;
 var net = require("net");
 var argv = require('optimist').argv;
 
-var HOST = argv.host?argv.host:'raspberrypi';
+var HOST = argv.host?argv.host:'localhost';
 var PORT = argv.port?argv.port:4304;
 console.log("Connecting to "+HOST+":"+PORT);
 var owfs = new Client(HOST, PORT);
@@ -11,11 +11,19 @@ owfs.dir('/', function(result){
 	console.log("dir",result);
 });
 
-owfs.read('/22.8CE2B3471711/temperature', function(result){
+owfs.get('/', function(result){
+	console.log("get",result);
+});
+
+owfs.getslash('/', function(result){
+	console.log("getslash",result);
+});
+
+owfs.read('/10.A7F1D92A82C8/temperature', function(result){
 	console.log("Result 1: "+result);
 })
 
-owfs.read('/10.67C6697351FF/temperature', function(result){
+owfs.read('/10.A7F1D92A82C8/temperature', function(result){
 	console.log("Result 2: "+result);
 });
 
