5e20dfa1de0dcf8a379a9a464785cc2c331fecb6
Nicholas Humfrey <njh@aelius.com>
2017-01-01 15:28:41 +0000

    Added test for #write()

--- test/basicTest.js
+++ test/basicTest.js
@@ -27,6 +27,22 @@ function communicationRead(value) {
     }]);
 }
 
+function communicationWrite(value) {
+    sendCommandStub = sandbox.stub(communicationStub, "sendCommand");
+    sendCommandStub.callsArgWith(1, null, [{
+        "header": {
+            "version": 0,
+            "payload": 0,
+            "ret": 0,
+            "controlflags": 32,
+            "size": 1,
+            "offset": 0
+        },
+        "payload": value
+    }]);
+}
+
+
 describe("Constructor", function() {
     it("should use host and port parameter", function(done) {
         communicationRead("23");
@@ -77,6 +93,18 @@ describe("#read()", function() {
         owfs.read("/some/path", function(error, value) {
             assert.ok(!error);
             assert.equal(value, 3.3434);
+            done();
+        });
+    });
+});
+
+describe("#write()", function () {
+    var owfs = new Client("blablub", 4304, communicationStub);
+    it("should write an integer", function (done) {
+        communicationWrite("");
+        owfs.write("/some/path", 1, function (error, messages) {
+            assert.ok(!error);
+            assert.equal(messages[0].header.ret, 0);
             done();
         });
     });
