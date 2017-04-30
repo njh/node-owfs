185f233fd91379358dc1b114a0786e7fe57d8c08
Nicholas Humfrey <njh@aelius.com>
2017-04-30 13:00:39 +0100

    Added basic example of getting a temperature

---
+++ examples/read-temperature.js
@@ -0,0 +1,18 @@
+/*
+
+  Example of getting a single temperature from owserver.
+
+*/
+
+'use strict';
+
+var Client = require('owfs').Client;
+var client = new Client('localhost', 4304); 
+
+client.read('/28.C040C4030000/temperature', function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
+});
