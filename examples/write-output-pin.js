15269c99f7db08d53e606f0c602a4d498f6e02b1
Nicholas Humfrey <njh@aelius.com>
2017-04-30 18:28:47 +0100

    Added example of writing to an output pin

---
+++ examples/write-output-pin.js
@@ -0,0 +1,18 @@
+/*
+
+  Example of writing to an DS2413 output pin.
+
+*/
+
+'use strict';
+
+var Client = require('owfs').Client;
+var client = new Client('localhost', 4304); 
+
+client.write('/3A.378A06000000/PIO.A', 1, function(err, message) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(message);
+    }
+});
