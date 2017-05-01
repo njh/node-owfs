88a64884fe3009296262b5bd2a8b52f953837586
Nicholas Humfrey <njh@aelius.com>
2017-05-01 22:20:40 +0100

    Changed the 'write' example to use 'true' instead of '1'

--- examples/write-output-pin.js
+++ examples/write-output-pin.js
@@ -9,7 +9,7 @@
 var Client = require('owfs').Client;
 var client = new Client('localhost', 4304); 
 
-client.write('/3A.378A06000000/PIO.A', 1, function(err, message) {
+client.write('/3A.378A06000000/PIO.A', true, function(err, message) {
     if (err) {
         console.log(err);
     } else {
